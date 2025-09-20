3507856f27b9ee0b0f21cbc25c1cdfdc09a002e8
liamdatt <liamdatt@gmail.com>
2025-09-19 21:39:55 -0500

    Team Fix

--- core/views.py
+++ core/views.py
@@ -1,56 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-from .models import SocialMedia, TeamMember, Service, CompanyInfo
+from django.shortcuts import render
 
 def home(request):
     """Home page view"""
-    company_info = CompanyInfo.objects.first()
-    social_media = SocialMedia.objects.filter(is_active=True)
-    context = {
-        'company_info': company_info,
-        'social_media': social_media,
-    }
-    return render(request, 'core/home.html', context)
+    return render(request, 'core/home.html')
 
 def about(request):
     """About Us page view"""
-    company_info = CompanyInfo.objects.first()
-    social_media = SocialMedia.objects.filter(is_active=True)
-    context = {
-        'company_info': company_info,
-        'social_media': social_media,
-    }
-    return render(request, 'core/about.html', context)
+    return render(request, 'core/about.html')
 
 def services(request):
     """Our Services page view"""
-    services = Service.objects.filter(is_active=True)
-    company_info = CompanyInfo.objects.first()
-    social_media = SocialMedia.objects.filter(is_active=True)
-    context = {
-        'services': services,
-        'company_info': company_info,
-        'social_media': social_media,
-    }
-    return render(request, 'core/services.html', context)
+    return render(request, 'core/services.html')
 
 def team(request):
     """Our Team page view"""
-    team_members = TeamMember.objects.filter(is_active=True)
-    company_info = CompanyInfo.objects.first()
-    social_media = SocialMedia.objects.filter(is_active=True)
-    context = {
-        'team_members': team_members,
-        'company_info': company_info,
-        'social_media': social_media,
-    }
-    return render(request, 'core/team.html', context)
+    return render(request, 'core/team.html')
 
 def contact(request):
     """Contact Us page view"""
-    company_info = CompanyInfo.objects.first()
-    social_media = SocialMedia.objects.filter(is_active=True)
-    context = {
-        'company_info': company_info,
-        'social_media': social_media,
-    }
-    return render(request, 'core/contact.html', context)
+    return render(request, 'core/contact.html')
